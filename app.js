60f7d56035ccc7f0eee35d3ddf90219ff2eb2d2a
timeraibra <itimera70@gmail.com>
2023-01-11 12:59:30 +0100

    parfum

---
+++ app.js
@@ -0,0 +1,11 @@
+// let openModalButton = document.getElementById("open-modal");
+// let closeModalButton = document.getElementById("close-modal");
+// let modal = document.getElementById("modal");
+
+// openModalButton.addEventListener("click", function() {
+//   modal.style.display = "block";
+// });
+
+// closeModalButton.addEventListener("click", function() {
+//   modal.style.display = "none";
+// });
